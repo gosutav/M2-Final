8ff570f2b29c5445e5e40ce73dbfd7822a585155
gosutav <gustavo.pinto.s@usach.cl>
2024-08-01 20:59:51 -0400

    p2 finalizado

--- Assets/JS/p1.js
+++ Assets/JS/p1.js
@@ -1,15 +1,13 @@
 var button = document.getElementById("convertir")
+
 button.addEventListener("click", function(){
       var USDamount = document.getElementById("USD")
       if(!isNaN(USDamount.value)){
       var CONVamount = USDamount.value*943.3 
-      var CLPamount = document.getElementById("CLP")
        document.getElementById("CLP").innerHTML = CONVamount+" CLP"
       }
       else{
             document.getElementById("CLP").innerHTML = "Por favor ingrese una suma válida"
-      }
- 
+      } 
 })
-
 
--- Assets/JS/p2.js
+++ Assets/JS/p2.js
@@ -1,19 +1,43 @@
+var button = document.getElementById("crear")
+
+
+button.addEventListener('click', crearPersonaje)
+
 class Character{
 
-    constructor(nomb, img, desc){
+    constructor(nomb, img){
 
         this.nombre = nomb;
         this.imagen = img;
-        this.descripcion = desc;
+        
+    }
+    
+}
+
+function crearPersonaje(){
+
+    var nombpj = document.getElementById("nomb-pj").value
+    var imglink = document.getElementById("img-pj").value
+
+    if(nombpj && imglink){  
+        
+        var char1 = new Character(nombpj, imglink)
+
+        const nuevoNombre = document.createElement('h2');
+        const nuevoImg    = document.createElement('img');
+        nuevoImg.setAttribute('src', char1.imagen);
+        nuevoImg.style.height = '300px'
+        nuevoImg.style.width = '200px'
+    
+        nuevoNombre.textContent = char1.nombre;
+    
+        document.getElementById("OnePiece").appendChild(nuevoNombre);
+        document.getElementById("OnePiece").appendChild(nuevoImg);
+    
+        console.log(char1)
 
     }
 
 }
 
-var char1 = new Character("Luffy", "Mugiwara", "Strawhat")
 
-console.log(char1)
-
-const elemento = document.createElement('h2')
-
-elemento.textContent = char1.nombre
